9a57cb8d5a10bdf04567e8b157ecd778c12f030b
AldiRvn <aldirvn4@gmail.com>
2020-05-03 16:57:26 +0700

    +rollQuote +covidCase function

--- main.js
+++ main.js
@@ -1,14 +1,39 @@
-alert('Hello, Welcome. This Only Mock Up.')
+// alert('Hello, Welcome. This web is on development.')
+covidCase()
+rollQuote()
 
-// Random Quote
-var request = new XMLHttpRequest(),
-    url = 'https://api.quotable.io/random'
-
-request.open('GET', url, true)
-request.onload = function() {
-    var data = JSON.parse(this.response)
-    document.getElementById('quote').innerHTML = '"'+data.content+'"'
-    document.getElementById('author').innerHTML = '- '+data.author
+function ajax(url, after) {
+    var request = new XMLHttpRequest()
+    
+    request.open('GET', url, true)
+    request.onload = function() {
+        after(this.response)
+    }
+    
+    request.send()
 }
 
-request.send()
+
+// Random Quote
+function rollQuote() {
+    document.getElementById('quote').innerHTML = 'Loading...'
+    document.getElementById('author').innerHTML = 'Please wait'
+
+    var url = 'https://api.quotable.io/random'
+    ajax(url, function(data) {
+        data = JSON.parse(data)
+        document.getElementById('quote').innerHTML = '"'+data.content+'"'
+        document.getElementById('author').innerHTML = '- '+data.author
+    })
+}
+
+// COVID-19 Case
+function covidCase() {
+    var url = 'https://api.kawalcorona.com/indonesia/'
+    ajax(url, function(data) {
+        data = JSON.parse(data)
+        document.querySelector('.case.confirmed').innerHTML = data[0].positif
+        document.querySelector('.case.recovered').innerHTML = data[0].sembuh
+        document.querySelector('.case.deaths').innerHTML = data[0].meninggal
+    })
+}
