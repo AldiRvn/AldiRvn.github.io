c1d38374a50e8af690b1ee28aba730e55ccb5b10
AldiRvn <aldirvn4@gmail.com>
2020-05-03 14:10:28 +0700

    get and show random quote from API

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-alert('Hello, Welcome. This Only Mock Up.');
+alert('Hello, Welcome. This Only Mock Up.')
+
+// Random Quote
+var request = new XMLHttpRequest(),
+    url = 'api.quotable.io/random'
+
+request.open('GET', url, true)
+request.onload = function() {
+    var data = JSON.parse(this.response)
+    document.getElementById('quote').innerHTML = data.content;
+}
+
+request.send()
